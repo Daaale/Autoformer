8063dfc1933e7feab3a753aae490eeee527579d4
Dale <172586304@qq.com>
2022-11-01 21:52:00 +0800

    add test

---
+++ test/test01.py
@@ -0,0 +1,16 @@
+import torch
+import torchvision
+import numpy
+import pandas
+import matplotlib
+import sklearn
+print(torch.__version__)
+print(torchvision.__version__)
+print(numpy.__version__)
+print(pandas.__version__)
+print(matplotlib.__version__)
+print(sklearn.__version__)
+print("HelloWorld")
+print("aaacbaa")
+
+
